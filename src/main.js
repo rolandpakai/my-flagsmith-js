50c5755791133e7d51a43159f24685ddbe175184
roland.pakai <roland.pakai@lightware.com>
2023-10-22 08:39:20 +0200

    Fix eslint issues

--- src/main.js
+++ src/main.js
@@ -1,38 +1,36 @@
-'use strict';
-
 import flagsmith from 'flagsmith';
 
 const environmentID = 'DCYBQgBcuRF86fmvgZc2os';
-const feature_name = 'test_feature';
+const featureName = 'test_feature';
 
 flagsmith.init({
   environmentID,
   cacheFlags: false,
-  //cacheOptions: {ttl:2, skipAPI:false},
-  //enableLogs: true,
-  //enableAnalytics: true, 
+  // cacheOptions: {ttl:2, skipAPI:false},
+  // enableLogs: true,
+  // enableAnalytics: true,
   identity: 'flagsmith_sample_user',
   onChange: (oldFlags, params) => {
-    console.log('oldFlags: ' + JSON.stringify(oldFlags));
-    console.log('params: ' + JSON.stringify(params));
+    console.log(`oldFlags: ${JSON.stringify(oldFlags)}`);
+    console.log(`params: ${JSON.stringify(params)}`);
 
     const flags = flagsmith.getAllFlags();
-    console.log("Received flags", flags)
+    console.log('Received flags', flags);
 
-    const has_feature = flagsmith.hasFeature(feature_name);
+    const hasFeature = flagsmith.hasFeature(featureName);
 
-    console.log('has_feature: ' + has_feature);
+    console.log(`has_feature: ${hasFeature}`);
 
-    if (has_feature) {
-      const value = flagsmith.getValue(feature_name);
-      console.log("value = " + value);
+    if (hasFeature) {
+      const value = flagsmith.getValue(featureName);
+      console.log(`value = ${value}`);
 
       // Check whether value has changed
-      const value_old = oldFlags[feature_name] && oldFlags[feature_name].value;
-      
-      if (value !== value_old) {
+      const valueOld = oldFlags[featureName] && oldFlags[featureName].value;
+
+      if (value !== valueOld) {
         // Value has changed, do something here
       }
     }
-  }
+  },
 });
